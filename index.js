fdc5a2e34ce7e91daa8330cdcd0feab92c458a36
bushra3112004 <pathanbushra006@gmail.com>
2025-05-15 15:27:18 +0530

    backend completedd

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import userRoute from './route/user.route.js';
 import cors from 'cors'
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 dotenv.config();
 const PORT=process.env.PORT || 4000; 
 const URI=process.env.MongoDBURI;
